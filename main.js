c339c3edccec59112caa431dee793f359041867d
wenhuiwu6 <wenhuiwu678@gmail.com>
2026-01-28 12:32:13 -0500

    aaa

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-function random() {}
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+console.log(getRandomInt(1, 11));
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+let number = getRandomInt(1, 11);
