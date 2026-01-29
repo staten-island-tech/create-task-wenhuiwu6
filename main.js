8adbf3df39cb2ead525936abd0a027517ed8fda0
wenhuiwu6 <wenhuiwu678@gmail.com>
2026-01-29 12:32:05 -0500

    qqq

--- main.js
+++ main.js
@@ -3,6 +3,19 @@ const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 function getRandomInt(min, max) {
   const minCeiled = Math.ceil(min);
   const maxFloored = Math.floor(max);
-  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled);
+  // The maximum is exclusive and the minimum is inclusive
 }
-console.log(getRandomInt(1, 11));
+const number = getRandomInt(1, 11);
+// console.log(number);
+
+const response = 3;
+
+function ppl_response(response) {
+  if (response === number) {
+    console.log("bingo! ur correct!");
+  }
+  if (response != number) {
+    console.log("oops. try again.");
+  }
+}
