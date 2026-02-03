838cb69945cac303e94c369800964480a1a4e78e
wenhuiwu6 <wenhuiwu678@gmail.com>
2026-02-02 22:19:08 -0500

    qwq

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+// function that chooses a random number from the array above
 function RanNum() {
   return numbers[Math.floor(Math.random() * numbers.length)];
 }
 let number = RanNum();
-console.log(number);
+console.log(number); // to see the random number
 
 function ppl_response(response) {
   const respond = document.getElementById("result");
@@ -17,6 +18,7 @@ function ppl_response(response) {
   }
 }
 
+// get what the user submit and convert it into number then checks if the number if its correct from the function above
 const form = document.getElementById("searchForm");
 form.addEventListener("submit", function (e) {
   e.preventDefault();
@@ -25,12 +27,12 @@ form.addEventListener("submit", function (e) {
   ppl_response(userAns);
 });
 
+//when the restart button is clicked it generates a new number from the RanNum function then clears the result messages from before
 const restart = document.getElementById("restart");
 if (restart) {
   restart.addEventListener("click", function () {
     number = RanNum();
     console.log(number);
     document.getElementById("result").innerHTML = "";
-    document.getElementById("title").value = "";
   });
 }
